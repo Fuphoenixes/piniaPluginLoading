df750a690ef7a73173ff225d90e007957fbef712
mohamadkhani <mohamadkhani@live.com>
2022-07-17 13:50:07 +0430

    fix: output type problem

--- src/index.ts
+++ src/index.ts
@@ -1,40 +1,42 @@
-import type { PiniaPluginContext } from 'pinia'
-import { ref, Ref } from 'vue-demi'
+import type { PiniaPluginContext } from "pinia";
+import { ref, Ref } from "vue-demi";
 
 export function PiniaLoading({ options, store }: PiniaPluginContext) {
   if (options.actions) {
-    const $loading: Record<string, Ref<boolean>> = {}
+    const $loading: Record<string, boolean> = {};
     Object.keys(options.actions).forEach((actionKey) => {
-      const originAction = options.actions[actionKey]
-      const action = function(this: unknown, ...args : unknown[]) {
-        const rtn = originAction.apply(this, args)
+      const originAction = options.actions[actionKey];
+      const action = function (this: unknown, ...args: unknown[]) {
+        const rtn = originAction.apply(this, args);
         if (rtn instanceof Promise) {
-          $loading[actionKey] = ref(false)
+          $loading[actionKey] = ref(false);
           return new Promise((resolve, reject) => {
-            $loading[actionKey].value = true
+            $loading[actionKey].value = true;
             rtn
               .then(resolve)
               .catch(reject)
               .finally(() => {
-                $loading[actionKey].value = false
-              })
-          })
+                $loading[actionKey].value = false;
+              });
+          });
         } else {
-          return rtn
+          return rtn;
         }
-      }
-      store[actionKey] = action
-    })
+      };
+      store[actionKey] = action;
+    });
 
-    store.$loading = $loading
+    store.$loading = $loading;
   }
 }
 
-declare module 'pinia' {
+declare module "pinia" {
   // eslint-disable-next-line
   export interface PiniaCustomProperties<Id, S, G, A> {
     $loading: {
-      [K in keyof A as A[K] extends (...args: any[]) => Promise<any> ? K : never]: Ref<boolean>;
-    }
+      [K in keyof A as A[K] extends (...args: any[]) => Promise<any>
+        ? K
+        : never]: boolean;
+    };
   }
 }
